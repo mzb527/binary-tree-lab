7a87f68124560a48f508bb05411dc104dd009fcb
mzb527 <mzb527@gmail.com>
2025-05-30 18:08:59 -0400

    Uploading Lab
    Found functions, tested, removed tests.

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -6,10 +6,20 @@ class TreeNode:
         self.left: Optional['TreeNode'] = None
         self.right: Optional['TreeNode'] = None
 
-# TODO: Implement the max_depth function
+# Function to compute the max depth of the binary tree
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if not root:
+        return 0
+    return max(max_depth(root.left), max_depth(root.right)) + 1
 
-# TODO: Implement the lowest_common_ancestor function
+# Function to find the lowest common ancestor in a BST
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    if not root:
+        return None
+
+    if p.val < root.val and q.val < root.val:
+        return lowest_common_ancestor(root.left, p, q)
+    elif p.val > root.val and q.val > root.val:
+        return lowest_common_ancestor(root.right, p, q)
+
+    return root  # If split occurs, root is the LCA
